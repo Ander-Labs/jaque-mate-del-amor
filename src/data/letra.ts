0ca2e035ec773f2a61a469e28383408c5d8be4d6
andersonruizr2 <andersonruizr2@gmail.com>
2024-11-12 15:21:44 +0000

    update file

---
+++ src/data/letra.ts
@@ -0,0 +1,31 @@
+export const LetraMusic = [
+  {
+    p1: "Rojo pasión, tu cabello bajo la luna Tu enigma me cautiva, dulce tortura Amistad profunda, un lazo que nos une Pero en mi interior, hay un amor que consume",
+  },
+  {
+    p2: "En un tablero de sueños, jugamos el destino Cada mirada es un movimiento divino Reina de corazones, me tienes en jaque mate En este juego de amor, no quiero retirarme ",
+  },
+  {
+    p3: "Eres mi reina, mi jaque mate En este juego de amor, mi corazón late ¿Sentirás lo mismo que yo? ¿O perderé esta partida, solo yo?",
+  },
+  {
+    p4: "Noches sin dormir, pensando en ti Mirando al cielo, buscando el destino en mí Si pudiera leer tu mente, descubriría Si este amor es mutuo o fantasía En cada mirada, busco una señal Un gesto, una palabra, algo real Mi corazón se arriesga, pero teme el final Quiero rendirme, pero en tus ojos no hay opción ",
+  },
+  {
+    p5: "Recuerdo aquella vez, bajo el cielo estrellado Cuando tus ojos se cruzaron con los míos Supe en ese instante que mi vida había cambiado Que eras todo lo que siempre había soñado ",
+  },
+
+  {
+    p6: "Eres mi reina, mi jaque mate En este juego de amor, mi corazón late ¿Sentirás lo mismo que yo? ¿O perderé esta partida, solo yo? ",
+  },
+
+  {
+    p7: "Noches sin dormir, pensando en ti Cada susurro al viento me trae tu perfil El miedo al rechazo me paraliza Pero en este juego, perderte me agoniza protagonista entre la esperanza y el temor de perderla ",
+  },
+  {
+    p8: "Eres mi reina, mi jaque mate En este juego de amor, mi corazón late ¿Sentirás lo mismo que yo? ¿O perderé esta partida, solo yo? ",
+  },
+  {
+    p9: "Me retiro del tablero… ¿o será que hay otro juego?",
+  },
+];
